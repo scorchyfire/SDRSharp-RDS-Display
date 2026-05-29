76ef3f4d6a349ce076d510ac4c821955998679ce
gary-husky <casey.maxwell017@gmail.com>
2026-05-29 13:18:37 -0400

    Add updated files for latest SDRSharp version

--- sdrplugins/RdsDisplay/build-and-deploy.bat
+++ sdrplugins/RdsDisplay/build-and-deploy.bat
@@ -9,8 +9,8 @@ if errorlevel 1 (
 )
 
 echo.
-echo Copying to SDRSharp...
-copy /y "%~dp0..\Release\net7.0-windows\SDRSharp.RdsDisplay.dll" "Z:\SDR Software-Other\sdrsharp-x86\Plugins\SDRSharp.RdsDisplay.dll"
+echo Copying to SDRSharp Plugins folder...
+copy /y "%~dp0..\Release\net9.0-windows\SDRSharp.RdsDisplay.dll" "%~dp0..\bin\Plugins\SDRSharp.RdsDisplay.dll"
 if errorlevel 1 (
     echo.
     echo COPY FAILED. Is SDRSharp still running?
@@ -18,6 +18,8 @@ if errorlevel 1 (
     exit /b 1
 )
 
+copy /y "%~dp0..\Release\net9.0-windows\pi_codes.json" "%~dp0..\bin\Plugins\pi_codes.json"
+
 echo.
-echo Done! Plugin deployed successfully.
+echo Done! Plugin deployed to sdrplugins\bin\Plugins successfully.
 pause
